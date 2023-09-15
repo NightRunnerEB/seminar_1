1e55ed00b22efe336d3bc8be32183aaa8b2d5c11
Evgeniy Bukharev <ebukharev_1@edu.hse.ru>
2023-09-15 18:25:57 +0300

    Initial Commit

---
+++ seminar_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar_1
+//
+//  Created by Евгений Бухарев on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
